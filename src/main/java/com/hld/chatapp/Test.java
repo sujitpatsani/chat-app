0a92df05b8dda13bac5875ad28f92bfbd8be99a5
sujitpatsani <sujit.patsani@gmail.com>
2024-04-23 23:10:29 +0530

    delete api key

--- src/main/java/com/hld/chatapp/Test.java
+++
@@ -1,8 +0,0 @@
-package com.hld.chatapp;
-
-public class Test {
-
-    public static void main(String[] args) {
-        String googleApikey=83878739389jhduhhggdfgfsuyt76g7767g77
-    }
-}
---
+++ src/main/java/com/hld/chatapp/Test2.java
@@ -0,0 +1,7 @@
+package com.hld.chatapp;
+
+public class Test2 {
+    public static void main(String[] args) {
+        static
+    }
+}
